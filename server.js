5d2eaad6a22a5228f57b0e9d6fbb47f56bc779df
Corey McDaniel <corey.r.mcdaniel@live.com>
2015-12-11 12:50:38 -0800

    Updated listen port for heroku server.

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var config = require('./config/config'),
 	express = require('./config/express');
 	
-var app = express();
+var app = express()
+var port = process.env.PORT || config.port;
 
-app.listen(config.port);
+app.listen(port);
 
 module.exports = app;
 
-console.log('Server running at http://localhost:' + config.port);
+console.log('Server running at http://localhost:' + port);
